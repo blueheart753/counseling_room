bd363737a4a3e9045584f7fc669c9da29235b867
kimminam <bluemind8520@gmail.com>
2023-05-30 22:57:11 +0900

    [fix] 예약 취소 버튼 클릭시 버튼 요소 제거

--- JavaScript/reservation.js
+++ JavaScript/reservation.js
@@ -5,7 +5,7 @@ const user_name = '김미남';
 reservtionButton.addEventListener('click', () => {
   const selectTime = select.options[select.selectedIndex].value;
   if (selectTime == 'select') {
-    alert(selectTime);
+    alert('시간대를 선택해주세요');
   } else {
     const result = document.querySelector('.Result');
     result.innerHTML = `${user_name}님 <br> ${selectTime} 상담 예약이 완료되었습니다.`;
