17fc00515f5409b9dce25b24e3d26faa99b8ac40
kimminam <bluemind8520@gmail.com>
2023-05-30 18:15:54 +0900

    [feat] popup js

--- JavaScript/reservation.js
+++ JavaScript/reservation.js
@@ -1,3 +1,30 @@
-const reservtionButton = document.querySelector(".reservation button");
+const reservtionButton = document.querySelector('.reservation button');
+const select = document.querySelector('#time');
+const popupLayer = document.querySelector('.popup');
+const user_name = '김미남';
+reservtionButton.addEventListener('click', () => {
+  const selectTime = select.options[select.selectedIndex].value;
+  if (selectTime == 'select') {
+    alert(selectTime);
+  } else {
+    const result = document.querySelector('.Result');
+    result.innerHTML = `${user_name}님 <br> ${selectTime} 상담 예약이 완료되었습니다.`;
+    popupLayer.style.display = 'flex';
+  }
+});
 
-reservtionButton.addEventListener("click", () => {});
+const popupBtn = document.querySelectorAll('.popupLayer button');
+
+popupBtn.forEach((el, idx) => {
+  el.addEventListener('click', () => {
+    switch (idx) {
+      case 0:
+        location.href = '../public/studentPage.html';
+        break;
+
+      default:
+        location.href = '../public/reservationCheck.html';
+        break;
+    }
+  });
+});
