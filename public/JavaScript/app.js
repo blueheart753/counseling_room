180312000ead01a5641859a9bcbf5e0d1276e31a
kimminam <bluemind8520@gmail.com>
2023-06-04 14:55:41 +0900

    file change

--- public/JavaScript/app.js
+++ public/JavaScript/app.js
@@ -3,7 +3,7 @@ const btns = document.querySelectorAll('button');
 btns.forEach((el, idx) => {
   el.addEventListener('click', () => {
     if (idx == 0) {
-      location.href = './studentPage.html';
+      location.href = '../studentPage.hbs';
     } else {
       location.href = './teacherLogin.html';
     }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: '상담실 | Counseling Room' });
+});
+
+router.get('/', function (req, res, next) {
+  res.render('studentPage', { title: '학생용 예약 사이트' });
 });
 
 module.exports = router;
